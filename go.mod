e129a3418ae01ab42deeb79ee0f2e4e18ae77822
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-01-27 17:53:41 +0800

    feat: cmd parser

---
+++ go.mod
@@ -0,0 +1,3 @@
+module patpat
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	cmdPtr := flag.String("cmd", "pwd", "command to run Java file")
+	testPtr := flag.String("test", "zoo", "specify test file")
+	flag.Parse()
+	fmt.Println("cmd:", *cmdPtr)
+	fmt.Println("test:", *testPtr)
+	fin, _ := os.Open("in.txt")
+	fans, _ := os.Open("ans.txt")
+	cin, _ := ioutil.ReadAll(fin)
+	cans, _ := ioutil.ReadAll(fans)
+	fmt.Println(string(cin), string(cans))
+}
