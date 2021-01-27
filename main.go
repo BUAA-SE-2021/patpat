c89f7e3982763cb90d444e3ef1eb19a5f4d70d0c
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-01-27 22:02:16 +0800

    feat: compile & exec java, redirect stdout

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"patpat/config"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
 )
 
 func main() {
@@ -15,5 +18,30 @@ func main() {
 	// cin, _ := ioutil.ReadAll(fin)
 	// cans, _ := ioutil.ReadAll(fans)
 	// fmt.Println(string(cin), string(cans))
-	config.FetchConfig()
+	// config.FetchConfig()
+
+	f, _ := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0755)
+	os.Stdout = f
+	os.Stderr = f
+	subProcess := exec.Command("java", "-classpath", "src", "Test") //Just for testing, replace with your subProcess
+
+	stdin, err := subProcess.StdinPipe()
+	if err != nil {
+		fmt.Println(err) //replace with logger, or anything you want
+	}
+	defer stdin.Close() // the doc says subProcess.Wait will close it, but I'm not sure, so I kept this line
+
+	// subProcess.Stdout = os.Stdout
+	// subProcess.Stderr = os.Stderr
+	subProcess.Stdout = f
+	subProcess.Stderr = f
+
+	fmt.Println("START COMPILE")              //for debug
+	if err = subProcess.Start(); err != nil { //Use start, not run
+		fmt.Println("An error occured: ", err) //replace with logger, or anything you want
+	}
+
+	io.WriteString(stdin, "QUIT\n")
+	subProcess.Wait()
+	fmt.Println("END COMPILE") //for debug
 }
