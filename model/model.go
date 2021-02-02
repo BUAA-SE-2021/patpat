103d0f74f51b42508b6f72125994c2a407f293e9
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-02-02 23:27:57 +0800

    chore: delete extra json name

--- model/model.go
+++ model/model.go
@@ -4,25 +4,25 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Sid      int    `json:"sid"`
-	Password string `gorm:"type:varchar(255)" json:"password"`
+	Sid      int
+	Password string `gorm:"type:varchar(255)"`
 }
 
 type JudgeResultUsual struct {
 	gorm.Model
-	Num    int    `json:"num"`
-	Sid    int    `json:"sid"`
-	Name   string `gorm:"type:varchar(255)" json:"name"`
-	Test   string `gorm:"type:varchar(255)" json:"test"`
-	Result int    `json:"result"`
+	Num    int
+	Sid    int
+	Name   string `gorm:"type:varchar(255)"`
+	Test   string `gorm:"type:varchar(255)"`
+	Result int
 }
 
 type JudgeResultFormal struct {
 	gorm.Model
-	Num    int    `json:"num"`
-	Sid    int    `json:"sid"`
-	Name   string `gorm:"type:varchar(255)" json:"name"`
-	Test   string `gorm:"type:varchar(255)" json:"test"`
-	Result int    `json:"result"`
-	Tag    string `gorm:"type:varchar(255)" json:"tag"`
+	Num    int
+	Sid    int
+	Name   string `gorm:"type:varchar(255)"`
+	Test   string `gorm:"type:varchar(255)"`
+	Result int
+	Tag    string `gorm:"type:varchar(255)"`
 }
