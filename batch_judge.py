6dcc3b3060f5699695058b216239a91aed1551df
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-02-03 14:51:07 +0800

    feat: batch judge script

---
+++ batch_judge.py
@@ -0,0 +1,14 @@
+from os import walk
+from zipfile import ZipFile
+import subprocess
+proj_num=input()
+base_addr='./'+proj_num
+_, _, zipfile_names = next(walk(base_addr))
+print(zipfile_names)
+print()
+for zipfile_name in zipfile_names:
+    with ZipFile(base_addr+'/'+zipfile_name, 'r') as zip_obj:
+         zip_obj.extractall(base_addr+'/'+zipfile_name[:-4])
+for zipfile_name in zipfile_names:
+    subprocess.call(['./bin/patpat-windows-64', 'ta', '-judge', zipfile_name[:-4]])
+    print()
